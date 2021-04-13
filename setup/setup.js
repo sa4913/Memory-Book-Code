8e7b2f1c8286f5b165579758ae68e3d761fc19f5
Sarah <sa4913@nyu.edu>
2021-04-13 09:56:15 +0800

    fixed nav bar

--- setup/setup.js
+++ setup/setup.js
@@ -40,7 +40,7 @@ function init(){
   var str = `
   <div id="nav-wrapper">
 
-     <nav id="page-nav"><a class="button `+ checkPage('cover') +`" href="cover.html">Cover</a><a class="button `+ checkPage('two-three') +`" href="two-three.html">2-3</a><a class="button `+ checkPage('four-five') +`" href="four-five.html">4-5</a><a class="button `+ checkPage('six-seven') +`" href="six-seven.html">6-7</a><a class="button `+ checkPage('eight-nine') +`" href="eight-nine.html">8-9</a><a class="button `+ checkPage('ten-eleven') +`" href="ten-eleven.html">10-11</a><a class="button `+ checkPage('twelve-thirteen') +`" href="twelve-thirteen.html">12-13</a><a class="button `+ checkPage('fourteen-fifteen') +`" href="fourteen-fifteen.html">14-15</a><a class="button `+ checkPage('back') +`" href="back.html">Back</a></nav>
+     <nav id="page-nav"><a class="button `+ checkPage('cover') +`" href="cover.html">Cover</a><a class="button `+ checkPage('two-three') +`" href="two-three.html">2-3</a><a class="button `+ checkPage('four-five') +`" href="four-five.html">4-5</a><a class="button `+ checkPage('six-seven') +`" href="six-seven.html">6-7</a><a class="button `+ checkPage('eight-nine') +`" href="eight-nine.html">8-9</a><a class="button `+ checkPage('ten-eleven') +`" href="ten-eleven.html">10-11</a><a class="button `+ checkPage('twelve-thirteen') +`" href="twelve-thirteen.html">Events</a><a class="button `+ checkPage('fourteen-fifteen') +`" href="fourteen-fifteen.html">Honorable Mentions</a><a class="button `+ checkPage('back') +`" href="back.html">Back</a></nav>
   </div>
   `;
   //<a class="button `+ checkPage('middlespread') +`" href="middlespread.html">Garbage</a>
