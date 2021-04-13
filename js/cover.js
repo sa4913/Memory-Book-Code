a1b90e8ece011a85aab597a416d08ab7ceba0ef2
Sarah <sa4913@nyu.edu>
2021-04-13 09:35:54 +0800

    Title sequence

--- js/cover.js
+++ js/cover.js
@@ -1,10 +1,10 @@
 //colors to be used
 
-let lemon = "F5D367";
-let aqua = "84D7DF";
-let flamingo = "EF7C9B";
-let mint = "A2D39B";
-let lavender = "D593FF";
+let lemon = "#F5D367";
+let aqua = "#84D7DF";
+let flamingo = "#EF7C9B";
+let mint = "#A2D39B";
+let lavender = "#D593FF";
 
 
 let viz = d3.select("#container")
@@ -26,7 +26,40 @@ viz.append("rect")
 viz.append("image")
   .attr("link:href", "JY-Logo.png")
   .attr("x", 250)
-  .attr("y", 450)
+  .attr("y", 550)
   .attr("width", 300)
   .attr("height", 300)
 ;
+
+viz.append("text")
+.text("Jinyang")
+.attr("font-size", 150)
+.attr("fill", lavender)
+.attr("x", 200)
+.attr("y", 250)
+;
+
+viz.append("text")
+.text("Memory Book")
+.attr("font-size", 100)
+.attr("fill", lavender)
+.attr("x", 110)
+.attr("y", 400)
+;
+// viz.append("text")
+// .text("Book")
+// .attr("font-size", 150)
+// .attr("fill", lavender)
+// .attr("x", 200)
+// .attr("y", 900)
+// ;
+viz.append("text")
+.text("Spring 2021")
+.attr("font-size", 125)
+.attr("fill", lavender)
+.attr("x", 100)
+.attr("y", 1050)
+;
+
+//
+//
